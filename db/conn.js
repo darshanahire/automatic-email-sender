2734572b043ce524791bce08e3ddb37078aaf386
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-08-03 19:28:36 +0530

    initial commit

--- db/conn.js
+++ db/conn.js
@@ -2,8 +2,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const uri = "mongodb://localhost:27017/automatic-email-sender";
-// const uri = "mongodb+srv://jwtuser:pass@11@cluster0.vzhly.mongodb.net/automatic-email-sender?";
+// const uri = "mongodb://localhost:27017/automatic-email-sender";
+const uri = "mongodb+srv://jwtuser:pass@11@cluster0.vzhly.mongodb.net/automatic-email-sender?";
 // const uri = "mongodb+srv://darshan123:darshan123@cluster0.bqisf.mongodb.net/automatic-email-sender?"
 mongoose.connect(uri, {
     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false
