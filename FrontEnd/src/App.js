1f989fe8186344ec1b945851b350e3731d1d8b9a
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-08-07 12:10:48 +0530

    bug fixed

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -35,7 +35,7 @@ const AuthRoute =({component:Component,...rest})=>(
 
 function App() {
   return (<Router>
-    <meta name="viewport" content="width=device-width, initial-scale= 1.0" />
+    
     <div className="App">
       <div>
       <nav className="navbar navbar-expand-lg navbar-light">
